9b53d061ad8645c1537b42485cb089bb9aa3b3b8
shan <shan@57blocks.com>
2024-05-24 14:30:46 +0800

    update examples

---
+++ packages/examples/src/drawdownAndPayback.ts
@@ -0,0 +1,39 @@
+import "dotenv/config";
+
+import {
+  drawdown,
+  makePayment,
+  POOL_NAME,
+  StellarNetwork,
+  StellarWallet,
+} from "@huma-shan/soroban-sdk";
+
+const main = async () => {
+  const wallet = new StellarWallet(process.env.TEST_PRIVATE_KEY!);
+  //100 USDC. Stellar USDC's decimal is 7
+  const borrowAmount = 100_0000000;
+
+  const drawdownResult = await drawdown(
+    POOL_NAME.Arf,
+    StellarNetwork.localnet,
+    wallet,
+    borrowAmount as any
+  );
+  console.log(
+    `Drawdown success. Tx hash: ${drawdownResult.sendTransactionResponse?.hash}`
+  );
+
+  const paymentAmount = 100_0000000;
+  const makePaymentResult = await makePayment(
+    POOL_NAME.Arf,
+    StellarNetwork.localnet,
+    wallet,
+    paymentAmount as any,
+    true
+  );
+  console.log(
+    `Payment success. Tx hash: ${makePaymentResult.sendTransactionResponse?.hash}`
+  );
+};
+
+main();
