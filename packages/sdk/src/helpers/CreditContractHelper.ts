1b546d856d066dc6eee4e95f9d8e8c5e311b4a8a
Michael Liu <michael@huma.finance>
2025-05-29 12:01:46 -0400

    Remove duplicate approve call

--- packages/sdk/src/helpers/CreditContractHelper.ts
+++ packages/sdk/src/helpers/CreditContractHelper.ts
@@ -202,16 +202,6 @@ export async function approveAllowanceForSentinel(
   network: StellarNetwork,
   wallet: StellarWallet,
 ) {
-  const totalDue = await getTotalDue(
-    poolName,
-    network,
-    wallet,
-    wallet.userInfo.publicKey,
-  )
-  if (totalDue === null) {
-    throw new Error('Could not find total due')
-  }
-
   const poolStorageContext = new TransactionContext(
     poolName,
     network,
@@ -293,19 +283,6 @@ export async function makePayment(
   principalOnly: boolean,
 ) {
   await approveAllowanceForSentinel(poolName, network, wallet)
-
-  const poolStorageContext = new TransactionContext(
-    poolName,
-    network,
-    wallet,
-    'poolStorage',
-  )
-  const { result: sentinel }: { result: string } = await sendTransaction({
-    context: poolStorageContext,
-    method: 'get_sentinel',
-  })
-
-  await approveSep41Allowance(poolName, network, wallet, sentinel)
 
   const poolCreditContext = new TransactionContext(
     poolName,
