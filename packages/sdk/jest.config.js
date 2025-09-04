e2613755e1d357a605b41bb700eaf47b236748a9
M <michael.liu95@gmail.com>
2025-09-04 11:40:26 -0400

    Update stellar sdk (#73)
    * Update stellar sdk
    
    * Remove console
    
    * Upgraded node
    
    * Fix tests

--- packages/sdk/jest.config.js
+++ packages/sdk/jest.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'jsdom',
+  testEnvironment: 'node',
   transform: {
     '^.+\\.ts?$': ['ts-jest'],
-    '^.+\\.(js|jsx)$': 'babel-jest',
   },
+  setupFilesAfterEnv: ['<rootDir>/tests/setup.js'],
   coverageReporters: ['json-summary', 'text', 'lcov'],
   transformIgnorePatterns: ['<rootDir>/node_modules/'],
 }
